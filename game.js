fa70a455d03ad6f24b760c8728cd1121c2c8ab9c
Ace <aceejtay3@gmail.com>
2026-03-07 20:47:08 -0500

    easier to read and understand. I also added a name pool for the Iraq insurgents, which was missing before. I also fixed a typo in the code where I accidentally wrote "army," instead of "army:" when defining the French Army in the eras array.

--- game.js
+++ game.js
@@ -2,6 +2,7 @@ const eras = [
     { year: "334 BC", army: "Macedonian Army", conflict: "Invasion of Persia" },
     { year: "334 BC", army: "Persian Army", conflict: "Invasion of Persia" },
     { year: "1415", army: "English Army", conflict: "Hundred Years War" },
+    { year: "1415", army: "French Army", conflict: "Hundred Years War" },
     { year: "1805", army: "French Grande Armée", conflict: "Napoleonic Wars" },
     { year: "1863", army: "Union Army", conflict: "American Civil War" },
     { year: "1944", army: "US Army", conflict: "World War 2" },
@@ -15,15 +16,21 @@ const roles = ["Infantry", "Cavalry", "Artillery", "Scout", "Engineer"]
 
 const names = ["Thomas", "Darius", "Emile", "Jakob", "Remy", "Aurelio", "John", "Marcus",]
 
+//name pool for each war era
+
+//Invasion of Persia names
 const macedonianNames = ["Demetrios", "Nikandros", "Diodoros", "Andronikos", "Philotas", "Nikandria", "Diogeneas", "Zenon", "Menon", "Aristion", "Dionysios", "Demetrios", "Apollonios", "Alexander", "Theodore", "Philon", "Ariston", "Amyntas", "Kleon", "Herakleides"]
 const persianNames = ["Darius", "Xerxes", "Artaxerxes", "Cyrus", "Cambyses", "Bessus", "Mithridates", "Oxyathres", "Smerdis", "Bardiya", "Atossa", "Parysatis", "Amestris", "Roxana", "Statira", "Sisygambis"]  
 
+// Hundred Years War names
 const Englandnames = ["William", "Henry", "Richard", "Edward", "John", "Thomas", "Geoffrey", "Robert", "Simon", "Walter", "Hugh", "Roger", "Stephen", "Geoffrey", "Adam", "Gilbert"]
 const Frenchnames = ["Jean", "Pierre", "Louis", "Jacques", "Michel", "Claude", "François", "Henri", "Philippe", "Gérard", "Alain", "Bernard", "René", "Pascal", "Sophie", "Isabelle"]
 
+// WW2 names
 const USAnames = ["James", "John", "Robert", "Michael", "William", "David", "Richard", "Joseph", "Charles", "Thomas", "Christopher", "Daniel", "Matthew", "Anthony"]
 const Germanames = ["Hans", "Friedrich", "Karl", "Heinrich", "Wilhelm", "Otto", "Ernst", "Franz", "Maximilian", "Ludwig", "Georg", "Johann"]
 
+// Civil war name pool. black names on both armies
 const USAUnionwhitenames = ["James", "John", "Robert", "Michael", "William", "David", "Richard", "Joseph", "Charles", "Thomas", "Christopher", "Daniel", "Matthew", "Anthony"]
 const USASouthernwhitenames = ["William", "James", "John", "Robert", "Charles", "Thomas", "George", "Henry", "Joseph", "Samuel", "Benjamin", "David", "Richard", "Edward"]  
 const Civilwarblacknames = ["Frederick", "Langston", "James", "George", "William", "Henry", "Samuel", "Joseph", "Benjamin", "Joshua", "Isaac", "Daniel", "Aaron", "Abraham", "Charles", "Edward", "Marcus", "Lewis", "David", "Jacob", "Peter", "Solomon", "Thomas", "Robert", "Richard"]
@@ -56,6 +63,7 @@ if (era.army === "Macedonian Army") {
 } else if (era.army === "Iraq insurgents") {
     namePool = Iraqnames
 }
+// soldier
 const soldier = {
     name: namePool[Math.floor(Math.random() * namePool.length)],
     role: role,
@@ -65,6 +73,7 @@ const soldier = {
     reputation: 50
 }
 
+// actual ui
 
 document.getElementById("game").innerHTML = `
     <h2>${soldier.name}</h2>
